6b31cf0678d6ac4e887adabb28299f3ae69340fa
oneadm <5748917+oneadm@user.noreply.gitee.com>
2022-06-10 21:32:28 +0800

    Initial commit

---
+++ src/main/java/com/example/delayed_msg/DelayedMsgApplication.java
@@ -0,0 +1,13 @@
+package com.example.delayed_msg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelayedMsgApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DelayedMsgApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/delayed_msg/DelayedMsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.delayed_msg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelayedMsgApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
